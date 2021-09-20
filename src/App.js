32aa5e460456f62c763cabef85d701a5e4ae2f39
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-09-20 20:31:04 +0530

    Added react-router-dom to App.js. Created a Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
