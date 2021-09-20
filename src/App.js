d56e5d2cf2007603c5e52570fb8713c35b711544
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-09-21 02:07:34 +0530

    Created other pages and added them to the router.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
+          <Redirect to='/' />
         </Switch>
       </BrowserRouter>
     </>
