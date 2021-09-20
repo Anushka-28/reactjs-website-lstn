442e0c64457870f2c2e924a0da1227d4efb325a4
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-09-20 21:35:59 +0530

    Modified the Navbar to be responsive using @media

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { ReactComponent as LstnLogo } from '../lstn_gold.svg';
 
 function Navbar() {
-  return (
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-            <div>
-                <Link to='/' className='navbar-logo'>
-                    {/* <img src={LstnLogo} alt='logo' className='navbar-logo-icon' /> */}
-                    <LstnLogo className='navbar-logo-icon' height='40' />
-                    lstn
-                </Link>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        <LstnLogo className='navbar-logo-icon' height='40' />
+                        lstn
+                    </Link>
+                </div>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fa fa-times' : 'fa fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>Sign Up</Link>
+                    </li>
+                </ul>
             </div>
-            <ul className='nav-menu'>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-links'>Home</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/services' className='nav-links'>Services</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/products' className='nav-links'>Products</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links'>Sign Up</Link>
-                </li>
-            </ul>
-        </div>
-      </nav>
+        </nav>
     </>
-  );
+    );
 }
 
 export default Navbar;
